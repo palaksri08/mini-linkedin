dbf72f4cbd0052cc46eb2986a59d94bf0451aa43
Palak Srivastava <palak.srivastava.777@gmail.com>
2025-08-07 21:02:14 +0530

    Final fixes and cleanup before deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
 };
+
 module.exports = nextConfig;
